d4654f7bda90a5520f3f38b24caaa0c9bae8f532
an <annekegh@yahoo.co.uk>
2012-11-17 15:09:21 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/bin/env/python
+
+if __name__ == "__main__":
+    from distutils.core import setup
+    from glob import glob
+
+    setup(name='mcdiff',
+        version='0.001',
+        description='Program to find diffusion parameters with Monte Carlo routine.',
+        #author='An Ghysels',
+        #author_email='An.Ghysels@UGent.be',
+        #url='http://molmod.ugent.be/code/',
+        package_dir = {'mcdiff': 'lib'},
+        packages = ['mcdiff'],
+        scripts=glob("scripts/*"),
+        classifiers=[
+            #'Development Status :: 3 - Alpha',
+            #'Environment :: Console',
+            #'Intended Audience :: Science/Research',
+            #'License :: OSI Approved :: GNU General Public License (GPL)',
+            #'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+        ],
+    )
+
