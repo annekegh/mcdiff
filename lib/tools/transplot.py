099d44165ae184bc624eb641c87f9af430989c38
an <annekegh@yahoo.co.uk>
2015-10-02 11:30:43 +0200

    delete example from contour plots

--- lib/tools/transplot.py
+++ lib/tools/transplot.py
@@ -12,25 +12,28 @@ from ffmc import read_all_transitions
 
 
 
-def plot_transition(transition,shift,dtc):
+def plot_transition(transition,shift,dtc,filename=None):
     import matplotlib.pyplot as plt
-    print transition.shape
+    print "transition",transition.shape
     trans = transition[0,:,:]
     n = len(trans)
     x = range(n)
     X, Y = np.meshgrid(x, x)
-    print X.shape, Y.shape, trans.shape
+    print "plotting",X.shape, Y.shape, trans.shape
     plt.figure()
-    plt.contour(X,Y,trans)
+    plt.contourf(X,Y,trans)
     plt.axis('equal')
     plt.title("lt=%f, dt=%f, dn=%i" %(shift*dtc,dtc,shift))
-    plt.savefig("contour.%i.png"%shift)
+    if filename==None:
+        filename = "contour.%i.png"%shift
+    plt.savefig(filename)
+    print "file written:",filename
 
-for shift in [1,10,100,1000,5000,]:
-    argv = ["%i" %shift,"../HexdWat/up1/pbctrans/transitions.nbins48.%i.pbc.dat" %shift]
-    dtc = 1.
-    print "Plotting...", argv[1]
-    print "lt=%f, dt=%f, dn=%i" %(shift*dtc,dtc,shift)
-    lagtimes,transition,bins = read_all_transitions(argv)
-    plot_transition(transition,shift,dtc)
+#for shift in [1,10,100,1000,5000,]:
+#    argv = ["%i" %shift,"../HexdWat/up1/pbctrans/transitions.nbins48.%i.pbc.dat" %shift]
+#    dtc = 1.
+#    print "Plotting...", argv[1]
+#    print "lt=%f, dt=%f, dn=%i" %(shift*dtc,dtc,shift)
+#    lagtimes,transition,bins = read_all_transitions(argv)
+#    plot_transition(transition,shift,dtc)
 
