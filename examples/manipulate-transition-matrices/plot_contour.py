abbb970ea04480e06808b2955aeaf873a8352d04
an <annekegh@yahoo.co.uk>
2018-09-21 13:50:32 +0200

    add and rewrite example for plotting contours of transition matrix

---
+++ examples/manipulate-transition-matrices/plot_contour.py
@@ -0,0 +1,14 @@
+"""How to plot the transition matrix (square)"""
+
+from mcdiff.tools.ffmc import read_transition_square
+from mcdiff.tools.transplot import plot_transition
+import numpy as np
+
+shift = 20  # lag time, or "dn"
+nbins = 100
+filename = "../data/HexdWat/transitions.nbins100.20.pbc.A.dat"
+
+print "Plotting...", filename
+trans,bins,Dt = read_transition_square(filename,nbins)
+plot_transition(trans,shift)
+
--- lib/tools/transplot.py
+++ lib/tools/transplot.py
@@ -1,39 +1,26 @@
 #!/usr/bin/env python
-#
-# copyright: Gerhard Hummer (NIH, July 2012)
-# adapted by An Ghysels (August 2012)
-#
 
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 
-from ffmc import read_all_transitions
+def plot_transition(trans,shift,figname=None):
+    if figname==None:
+        figname = "contour.%i.png"%shift
 
-
-
-def plot_transition(transition,shift,dtc,filename=None):
     import matplotlib.pyplot as plt
-    print "transition",transition.shape
-    trans = transition[0,:,:]
     n = len(trans)
     x = range(n)
     X, Y = np.meshgrid(x, x)
-    print "plotting",X.shape, Y.shape, trans.shape
+    #print "plotting",X.shape, Y.shape, trans.shape
     plt.figure()
     plt.contourf(X,Y,trans)
     plt.axis('equal')
-    plt.title("lt=%f, dt=%f, dn=%i" %(shift*dtc,dtc,shift))
-    if filename==None:
-        filename = "contour.%i.png"%shift
-    plt.savefig(filename)
-    print "file written:",filename
+    plt.title("dn=%i" %shift)
+    plt.savefig(figname)
+    print "file written:",figname
 
-#for shift in [1,10,100,1000,5000,]:
-#    argv = ["%i" %shift,"../HexdWat/up1/pbctrans/transitions.nbins48.%i.pbc.dat" %shift]
-#    dtc = 1.
-#    print "Plotting...", argv[1]
-#    print "lt=%f, dt=%f, dn=%i" %(shift*dtc,dtc,shift)
-#    lagtimes,transition,bins = read_all_transitions(argv)
-#    plot_transition(transition,shift,dtc)
-
+    #print "transition",transition.shape
+    # in case it is needed to convert the format:
+    #trans = transition[0,:,:]
+    #plt.title("lt=%f, dt=%f, dn=%i" %(shift*dtc,dtc,shift))
